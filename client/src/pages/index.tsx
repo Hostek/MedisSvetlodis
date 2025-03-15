7a182be53df9821fd8d9c7137eb55aac611d5ed8
Hostek <michalpl245674477@gmail.com>
2025-03-15 19:11:58 +0100

    can create message

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,46 @@
 "use client"
+import { useCreateMessageMutation } from "@/generated/graphql"
 import { createUrqlClient } from "@/utils/createUrqlClient"
 import { NextPage } from "next"
 import { withUrqlClient } from "next-urql"
+import { useCallback, useState } from "react"
 
 const Page: NextPage = () => {
-    return <div>Hello World!</div>
+    const [content, setContent] = useState("")
+    const [{ fetching }, createMessage] = useCreateMessageMutation()
+
+    const handleSubmit = useCallback<React.FormEventHandler<HTMLFormElement>>(
+        async (e) => {
+            e.preventDefault()
+
+            const res = await createMessage({
+                content,
+                creatorId: 1,
+            })
+
+            console.log({ res })
+        },
+        [createMessage, content]
+    )
+
+    return (
+        <div>
+            <div>fetching: {String(fetching)}</div>
+            <form onSubmit={handleSubmit}>
+                Content msg:
+                <input
+                    value={content}
+                    onChange={(e) => setContent(e.currentTarget.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+
+            <div>
+                Messages:
+                <div></div>
+            </div>
+        </div>
+    )
 }
 
 export default withUrqlClient(createUrqlClient)(Page)
