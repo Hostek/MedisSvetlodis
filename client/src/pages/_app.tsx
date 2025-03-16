bfdc2c570d63e0f901f0738617f9add5be1cbdfc
Hostek <michalpl245674477@gmail.com>
2025-03-16 18:52:15 +0100

    add next-themes

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
 import { HeroUIProvider } from "@heroui/react"
+import { ThemeProvider } from "next-themes"
+import type { AppProps } from "next/app"
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <HeroUIProvider>
-            <Component {...pageProps} />
-        </HeroUIProvider>
+        <ThemeProvider defaultTheme="dark">
+            <HeroUIProvider>
+                <Component {...pageProps} />
+            </HeroUIProvider>
+        </ThemeProvider>
     )
 }
