49be8d84294beee9ef63178573e5367b003618c7
Hostek <michalpl245674477@gmail.com>
2025-03-24 19:43:01 +0100

    Export UpdateUsername to it's own file

--- client/src/pages/user/settings.tsx
+++ client/src/pages/user/settings.tsx
@@ -1,29 +1,13 @@
 "use client"
 import ChangePasswordTab from "@/components/settings/ChangePasswordTab"
-import { RED_COLOR } from "@/constants"
-import {
-    useCreateDefaultTokensMutation,
-    useUpdateUsernameMutation,
-} from "@/generated/graphql"
+import UpdateUsername from "@/components/settings/UpdateUsername"
+import { useCreateDefaultTokensMutation } from "@/generated/graphql"
 import { useIsAuth } from "@/hooks/isAuth"
-import { HandleSubmit } from "@/types"
 import { createUrqlClient } from "@/utils/createUrqlClient"
-import { MySwal } from "@/utils/MySwal"
-import {
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Divider,
-    Form,
-    Input,
-    Tab,
-    Tabs,
-} from "@heroui/react"
-import { errors, getUsernameError } from "@hostek/shared"
+import { Button, Divider, Tab, Tabs } from "@heroui/react"
 import { NextPage } from "next"
 import { withUrqlClient } from "next-urql"
-import { useCallback, useState } from "react"
+import { useState } from "react"
 import { ArrowLeft } from "react-feather"
 
 const Page: NextPage = () => {
@@ -31,69 +15,8 @@ const Page: NextPage = () => {
 
     const [showIdentifier, setShowIdentifier] = useState(false)
 
-    const [username, setUsername] = useState("")
-
-    const [updateUsernameError, setUpdateUsernameError] = useState<
-        string | null
-    >(null)
-
-    const [{ fetching: updateUsernameFetching }, updateUsername] =
-        useUpdateUsernameMutation()
-
     const [{ fetching: createDefaultTokensFetching }, createDefaultTokens] =
         useCreateDefaultTokensMutation()
-
-    const handleUsernameUpdate = useCallback<HandleSubmit>(
-        async (e) => {
-            e.preventDefault()
-
-            if (!user) return
-
-            const result = await MySwal.fire({
-                title: "Are you sure?",
-                text: "This operation is irreversible and will consume your username update attempt (if all are used you won't be able to change username ever again)",
-                icon: "warning",
-                theme: "dark",
-                showCancelButton: true,
-                confirmButtonText: "Yes, Change my username",
-                cancelButtonText: "Cancel",
-                confirmButtonColor: RED_COLOR,
-            })
-
-            if (!result.isConfirmed) return
-
-            setUpdateUsernameError(null)
-
-            const areErrors = getUsernameError(username)
-            if (areErrors) {
-                setUpdateUsernameError(areErrors)
-                return
-            }
-
-            const res = await updateUsername({
-                newUsername: username,
-            })
-
-            if (!res.data || res.error) {
-                setUpdateUsernameError(errors.unknownError)
-                return
-            }
-
-            if (res.data.updateUsername?.message) {
-                setUpdateUsernameError(res.data.updateUsername.message)
-            }
-
-            user.updateUsernameAttempts--
-
-            MySwal.fire({
-                title: "Success",
-                text: "Successfully changed username!",
-                icon: "success",
-                theme: "dark",
-            })
-        },
-        [username, updateUsername, user]
-    )
 
     if (!user) {
         return null
@@ -109,44 +32,11 @@ const Page: NextPage = () => {
 
             <Tabs aria-label="Settings options">
                 <Tab key="username" title="Change Username">
-                    <Card>
-                        <CardHeader className="font-bold">
-                            Change Username |
-                            <span className="text-gray-400 mx-1">
-                                (Available attempts:{" "}
-                                {user.updateUsernameAttempts})
-                            </span>
-                        </CardHeader>
-                        <Divider />
-                        <CardBody className="flex flex-col gap-4">
-                            <Form
-                                className="space-y-2"
-                                onSubmit={handleUsernameUpdate}
-                            >
-                                <Input
-                                    label="New Username"
-                                    placeholder="Enter new username"
-                                    value={username}
-                                    onChange={(e) =>
-                                        setUsername(e.target.value)
-                                    }
-                                />
-                                {updateUsernameError && (
-                                    <div className="w-full text-red-500">
-                                        Error: {updateUsernameError}
-                                    </div>
-                                )}
-                                <Button
-                                    type="submit"
-                                    color="primary"
-                                    className="w-full"
-                                    disabled={updateUsernameFetching}
-                                >
-                                    Update Username
-                                </Button>
-                            </Form>
-                        </CardBody>
-                    </Card>
+                    <UpdateUsername
+                        updateUsernameAttempts={user.updateUsernameAttempts}
+                        l={!!user}
+                        x={() => user.updateUsernameAttempts--}
+                    />
                 </Tab>
 
                 <Tab key="password" title="Change Password">
