9117d06fe6f263a5608c84a0f7ff58bb39a9bdc2
Hostek <michalpl245674477@gmail.com>
2025-03-24 21:00:22 +0100

    Add /temp/icons page

---
+++ client/src/pages/temp/icons.tsx
@@ -0,0 +1,43 @@
+"use client"
+import BlockIcon from "@/components/icons/BlockIcon"
+import { IconProvider } from "@/context/IconContext"
+import { SvgWrapperOptions } from "@/types"
+import { NextPage } from "next"
+
+export const icons: React.NamedExoticComponent<SvgWrapperOptions>[] = [
+    BlockIcon,
+]
+
+export const LocalIcons = () => {
+    return (
+        <>
+            {icons.map((Icon, i) => (
+                <Icon key={i} />
+            ))}
+        </>
+    )
+}
+
+const Page: NextPage = () => {
+    return (
+        <div style={{ background: "lightblue" }}>
+            <IconProvider width={25} height={25}>
+                <LocalIcons />
+            </IconProvider>
+            <hr />
+            <IconProvider width={50} height={50}>
+                <LocalIcons />
+            </IconProvider>
+            <hr />
+            <IconProvider width={75} height={75}>
+                <LocalIcons />
+            </IconProvider>
+            <hr />
+            <IconProvider width={100} height={100}>
+                <LocalIcons />
+            </IconProvider>
+        </div>
+    )
+}
+
+export default Page
