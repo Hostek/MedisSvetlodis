5a9d4dc0854272adbf4d29e69d3f1d8c37869d23
Hostek <michalpl245674477@gmail.com>
2025-04-07 21:10:37 +0200

    update blockUser –> auto-rejects friend requests of blocked users

--- server/src/resolvers/block.ts
+++ server/src/resolvers/block.ts
@@ -1,9 +1,13 @@
-import { Resolver, Mutation, Arg, Ctx, UseMiddleware } from "type-graphql"
-import { Block } from "../entities/Block.js"
-import { FieldError, MyContext } from "../types.js"
-import { isAuth } from "../middleware/isAuth.js"
 import { errors } from "@hostek/shared"
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql"
+import { In } from "typeorm"
+import { AppDataSource } from "../DataSource.js"
+import { Block } from "../entities/Block.js"
+import { FriendRequests } from "../entities/FriendsRequests.js"
 import { User } from "../entities/User.js"
+import { isAuth } from "../middleware/isAuth.js"
+import { FieldError, MyContext } from "../types.js"
+import { FriendRequestToken } from "../entities/FriendRequestToken.js"
 
 @Resolver()
 export class BlockResolver {
@@ -21,9 +25,34 @@ export class BlockResolver {
         if (!userExists) return { message: errors.userNotFound }
 
         try {
-            await Block.insert({
-                blockerId: ctx.req.session.userId,
-                blockedId: userId,
+            await AppDataSource.transaction(async (tm) => {
+                await tm.getRepository(Block).insert({
+                    blockerId: ctx.req.session.userId,
+                    blockedId: userId,
+                })
+
+                const requestTokens = await tm
+                    .getRepository(FriendRequestToken)
+                    .find({
+                        where: {
+                            userId: In([ctx.req.session.userId, userId]),
+                        },
+                    })
+
+                const ids = requestTokens.map((rtoken) => rtoken.id)
+
+                // auto-reject friend requests
+                await tm.getRepository(FriendRequests).update(
+                    {
+                        status: "pending",
+                        // senderId: Or(Equal(ctx.req.session.userId), Equal(userId)),
+                        senderId: In([ctx.req.session.userId, userId]),
+                        requestTokenId: In(ids),
+                    },
+                    {
+                        status: "rejected",
+                    }
+                )
             })
         } catch (error) {
             // postgres duplicate error
