354e41c594dd9f53dd8d652bfd4c6bbc2b43821f
Hostek <michalpl245674477@gmail.com>
2025-04-07 21:15:50 +0200

    update blockUser –> use single query instead of two queries

--- server/src/resolvers/block.ts
+++ server/src/resolvers/block.ts
@@ -1,13 +1,11 @@
 import { errors } from "@hostek/shared"
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql"
-import { In } from "typeorm"
 import { AppDataSource } from "../DataSource.js"
 import { Block } from "../entities/Block.js"
 import { FriendRequests } from "../entities/FriendsRequests.js"
 import { User } from "../entities/User.js"
 import { isAuth } from "../middleware/isAuth.js"
 import { FieldError, MyContext } from "../types.js"
-import { FriendRequestToken } from "../entities/FriendRequestToken.js"
 
 @Resolver()
 export class BlockResolver {
@@ -31,28 +29,22 @@ export class BlockResolver {
                     blockedId: userId,
                 })
 
-                const requestTokens = await tm
-                    .getRepository(FriendRequestToken)
-                    .find({
-                        where: {
-                            userId: In([ctx.req.session.userId, userId]),
-                        },
-                    })
-
-                const ids = requestTokens.map((rtoken) => rtoken.id)
-
                 // auto-reject friend requests
-                await tm.getRepository(FriendRequests).update(
-                    {
-                        status: "pending",
-                        // senderId: Or(Equal(ctx.req.session.userId), Equal(userId)),
-                        senderId: In([ctx.req.session.userId, userId]),
-                        requestTokenId: In(ids),
-                    },
-                    {
-                        status: "rejected",
-                    }
-                )
+                await tm
+                    .getRepository(FriendRequests)
+                    .createQueryBuilder()
+                    .update()
+                    .set({ status: "rejected" })
+                    .where("status = :status", { status: "pending" })
+                    .andWhere("senderId IN (:...senderIds)", {
+                        senderIds: [ctx.req.session.userId, userId],
+                    })
+                    .andWhere(
+                        "requestTokenId IN " +
+                            "(SELECT id FROM friend_request_token WHERE userId IN (:...userIds))",
+                        { userIds: [ctx.req.session.userId, userId] }
+                    )
+                    .execute()
             })
         } catch (error) {
             // postgres duplicate error
