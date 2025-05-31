1688fc6bfb4643c600c571215fb98e6ac527f0c9
Hostek <michalpl245674477@gmail.com>
2025-05-31 20:34:02 +0200

    in api/temp.tsx make not accesible when in prod

--- client/src/pages/api/temp.ts
+++ client/src/pages/api/temp.ts
@@ -7,6 +7,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (process.env.NODE_ENV === "production") {
+        return res.status(404).json({ error: "Not Found" })
+    }
+
     res.status(200).json({
         value: print(LoginDocument),
         tester: generateOAuthProof(
