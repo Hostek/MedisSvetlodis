15c6a3638e1e94031b518a8e86af89ad718eb2b4
Hostek <michalpl245674477@gmail.com>
2025-05-31 21:06:53 +0200

    add beautiful FriendCard

--- client/src/components/friends/FriendCard.tsx
+++ client/src/components/friends/FriendCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { FriendListEdge } from "@/types"
+import { Card } from "@heroui/react"
 import React from "react"
 
 interface FriendCardProps {
@@ -8,9 +9,21 @@ interface FriendCardProps {
 
 const FriendCard: React.FC<FriendCardProps> = ({ edge: { node } }) => {
     return (
-        <div className="friend-item">
-            {node.identifier} | @{node.username}
-        </div>
+        <Card className="my-7 py-5 px-2">
+            <div className="flex">
+                <div className="w-24">{/*Left*/}</div>
+                <div className="flex-1">
+                    <div className="flex flex-col">
+                        <p className="text-lg font-bold">@{node.username}</p>
+                        <p className="text-small text-gray-500">
+                            {node.identifier}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-24">{/*Right*/}</div>
+            </div>
+            {/*  */}
+        </Card>
     )
 }
 
